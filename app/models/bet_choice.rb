e77a4196284dff2c1bcf0ed2895db03883604583
Finn Glöe <login@amshaegar.de>
2015-04-05 11:31:52 +0000

    fixed foreign key

--- app/models/bet_choice.rb
+++ app/models/bet_choice.rb
@@ -1,3 +1,3 @@
 class BetChoice < ActiveRecord::Base
-  belongs_to :type, class_name: 'BetType'
+  belongs_to :type, class_name: 'BetType', foreign_key: 'type_id'
 end
--- app/models/bet_type.rb
+++ app/models/bet_type.rb
@@ -1,3 +1,3 @@
 class BetType < ActiveRecord::Base
-  has_many :choices, class_name: 'BetChoice'
+  has_many :choices, class_name: 'BetChoice', foreign_key: 'type_id'
 end
