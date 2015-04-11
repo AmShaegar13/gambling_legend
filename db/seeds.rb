9881bff0c7dbe261c2abfc9a947d651d3e4450de
Finn Glöe <login@amshaegar.de>
2015-04-11 21:39:53 +0000

    reordered seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,22 +13,19 @@ WinnerBet.create! label: 'TEAM_WIN' do |type|
   BetChoice.create! type: type, label: 'TEAM_RED'
 end
 
+BaronsBet.create! label: 'BARONS_KILLED' do |type|
+  BetChoice.create! type: type, label: 'NO_BARONS'
+  BetChoice.create! type: type, label: 'BARONS_1'
+  BetChoice.create! type: type, label: 'BARONS_2'
+  BetChoice.create! type: type, label: 'BARONS_3+'
+end
+
 MultiKillBet.create! label: 'LARGEST_MULTIKILL' do |type|
   BetChoice.create! type: type, label: 'MULTIKILL_1'
   BetChoice.create! type: type, label: 'MULTIKILL_2'
   BetChoice.create! type: type, label: 'MULTIKILL_3'
   BetChoice.create! type: type, label: 'MULTIKILL_4'
   BetChoice.create! type: type, label: 'MULTIKILL_5'
-end
-
-KillingSpreeBet.create! label: 'LARGEST_KILLING_SPREE' do |type|
-  BetChoice.create! type: type, label: 'NO_KILLING_SPREE'
-  BetChoice.create! type: type, label: 'KILLING_SPREE_3'
-  BetChoice.create! type: type, label: 'KILLING_SPREE_4'
-  BetChoice.create! type: type, label: 'KILLING_SPREE_5'
-  BetChoice.create! type: type, label: 'KILLING_SPREE_6'
-  BetChoice.create! type: type, label: 'KILLING_SPREE_7'
-  BetChoice.create! type: type, label: 'KILLING_SPREE_8+'
 end
 
 DragonsBet.create! label: 'DRAGONS_KILLED' do |type|
@@ -40,9 +37,12 @@ DragonsBet.create! label: 'DRAGONS_KILLED' do |type|
   BetChoice.create! type: type, label: 'DRAGONS_5+'
 end
 
-BaronsBet.create! label: 'BARONS_KILLED' do |type|
-  BetChoice.create! type: type, label: 'NO_BARONS'
-  BetChoice.create! type: type, label: 'BARONS_1'
-  BetChoice.create! type: type, label: 'BARONS_2'
-  BetChoice.create! type: type, label: 'BARONS_3+'
+KillingSpreeBet.create! label: 'LARGEST_KILLING_SPREE' do |type|
+  BetChoice.create! type: type, label: 'NO_KILLING_SPREE'
+  BetChoice.create! type: type, label: 'KILLING_SPREE_3'
+  BetChoice.create! type: type, label: 'KILLING_SPREE_4'
+  BetChoice.create! type: type, label: 'KILLING_SPREE_5'
+  BetChoice.create! type: type, label: 'KILLING_SPREE_6'
+  BetChoice.create! type: type, label: 'KILLING_SPREE_7'
+  BetChoice.create! type: type, label: 'KILLING_SPREE_8+'
 end
