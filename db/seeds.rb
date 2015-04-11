c66bc5419d3e1aec864b5ef6fe7faba1fdcb47e3
Finn Glöe <login@amshaegar.de>
2015-04-11 22:45:07 +0000

    added freigeist to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 User.create! name: 'AmShaegar', email: 'login@email.com', password: 'asdasd', last_refill: Time.now, balance: 1000
+User.create! name: 'Freigeist', email: 'your@email.com', password: 'asdasd', last_refill: Time.now, balance: 1000
 
 WinnerBet.create! label: 'TEAM_WIN' do |type|
   BetChoice.create! type: type, label: 'TEAM_BLUE'
