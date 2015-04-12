b74071af9742d2392b514fca4b1541fc54308f0e
Finn Glöe <login@amshaegar.de>
2015-04-12 14:02:23 +0000

    fixed order of choices/types

--- app/models/bet_choice.rb
+++ app/models/bet_choice.rb
@@ -1,6 +1,8 @@
 class BetChoice < ActiveRecord::Base
   belongs_to :type, class_name: 'BetType', foreign_key: 'type_id'
 
+  default_scope { order:id }
+
   def odds
     return @odd unless @odds.nil?
 
--- app/models/bet_type.rb
+++ app/models/bet_type.rb
@@ -1,6 +1,8 @@
 class BetType < ActiveRecord::Base
   has_many :choices, class_name: 'BetChoice', foreign_key: 'type_id'
 
+  default_scope { order:id }
+
   def process!(bet, match)
     raise "Don't know how to process #{self.class.name}"
   end
