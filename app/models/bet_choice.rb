378f2b2b2bca9a8416368d53a9b8350c349c2751
Finn Glöe <login@amshaegar.de>
2015-04-16 06:27:10 +0000

    changed calculation of odds

--- app/models/bet_choice.rb
+++ app/models/bet_choice.rb
@@ -4,11 +4,10 @@ class BetChoice < ActiveRecord::Base
   default_scope { order:id }
 
   def odds
-    choices = type.choices.count
     matches = Match.where('? IN (winner_id, barons_id, multi_kill_id, dragons_id, killing_spree_id)', self.id).count
     matches_sum = Match.count
     matches_sum = 1 if matches_sum == 0
 
-    1 + choices * (1 - matches.to_f/matches_sum)
+    matches_sum.to_f/matches
   end
 end
